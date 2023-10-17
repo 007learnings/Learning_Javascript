531f796d249c47129413428c014174b0e666ad2d
Vikrant-1 <vikrantkainth090699@gmail.com>
2023-10-17 22:09:54 +0530

    detaill added for memory

--- DataTypes/memory.js
+++ DataTypes/memory.js
@@ -13,5 +13,9 @@ console.log({a,b});
 let user = {name:'vikarnt'};
 let student = user;
 
+// primitive types are directly store  -> string , number , null, undefined , boolean , bigInt
+
+
+//  nonprimitive type store reference location
 // user :  referenece if of location where {name:'vikarnt'} is stored
 // student :referenece if of location where {name:'vikarnt'} is stored
