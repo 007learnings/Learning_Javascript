3f173d118be17603e286483cc080c30e7df8ae56
Vikrant-1 <vikrantkainth090699@gmail.com>
2023-10-21 21:20:22 +0530

    complete strings

--- DataTypes/jsString.js
+++ DataTypes/jsString.js
@@ -1,7 +1,7 @@
 //  change the first value which find and return a new value
 // let newStr = 'HelloSheraG';
 // let line = 'Hello my name is gaurav';
-// let temp =newStr.replace('l','i');
+// let temp =newStr.replace('ll','');
 // console.log({newStr,temp});
 
 //  Split a string into substrings using the specified separator and return them as an array.
@@ -14,7 +14,7 @@
 // let line = 'Hello my name is gaurav';
 // line.length
 
-let newStr = 'HelloSheraG';
+// let newStr = "HelloSheraG";
 // let line = 'Hello my name is gaurav';
 // newStr.toLowerCase()
 // newStr.toUpperCase()
@@ -22,37 +22,56 @@ let newStr = 'HelloSheraG';
 // newStr =newStr.toLowerCase()
 // console.log(newStr);
 
-
 // let newStr = 'HelloSheraG';
 // let line = 'Hello my name is gaurav';
 // console.log(line.charAt(22)); // start from 0
 
-
 // let line = ' ello my name is gaurav';
-// console.log(line.charCodeAt(0))
+// console.log(line.charCodeAt('A'))
 
 // let line = ' ello my name is gaurav';
 // console.log(line.codePointAt(0))
 
 // let line = 'Hello my name is gaurav';
-// line =line.concat()
+// line =line.concat('Hello2)
 
 // console.log(line);
 
-let line = 'Hello my name is ellrav';
+// let line = 'Hello my name is ellrav';
 // console.log(line.endsWith('Hello my name is gaurav'));
 
 // console.log(line.startsWith('Hello'));
 
 // console.log(line.includes('ell',5)) // position where to start seaching
 
-// if not value then return -1 , 
+// if not value then return -1 ,
 // console.log(line.indexOf('e',2))// position where to start seaching
 
-// start search from last 
+// start search from last
 // console.log(line.lastIndexOf('e',5)); // return last value
 
 // console.log(line.padEnd(22,'tt'));
 // console.log(line.padStart(30,'tt'));
 
 // console.log(line.repeat(2))
+
+// console.log(line.search('Hello'))
+// let line = "  Hell omynameiseHello ";
+
+// console.log(line.slice(2)); // we can use negative  , start  < end (must) ,
+
+// console.log(line.substring(2)); // we can not use negative  ,  start  < end (not must , it will swap the elements if start is greater than end)
+
+
+// console.log(line.toLocaleLowerCase())
+
+// console.log(line.toLocaleUpperCase());
+
+// console.log(line.toString()); // return a string
+
+
+// console.log(line.trim());  remove start and end whitespace
+
+// console.log(line.trimEnd()); remove white space from end
+
+// console.log(line.trimStart()); // remove white space from start
